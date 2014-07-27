b4495dd21d06555eb447c1aa09bbc736a2c5bf31
Craig <craig.treptow@gmail.com>
2014-07-27 07:57:35 -0500

    fixed it up a bit with a comment

--- ex1.rb
+++ ex1.rb
@@ -1,8 +1,12 @@
+#!/usr/bin/env ruby
+
+# this octothorpe makes this line a comment
+
 puts "Hello World!"
 puts "Hello Again"
 puts "I like typing this."
 puts "This is fun."
 puts "Yay! Printing."
-puts "I'd must rather you 'non'."
+puts "I'd must rather you 'not'."
 puts 'I "said" do not touch this.'
 
